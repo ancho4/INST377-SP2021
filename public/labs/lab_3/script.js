64734bea1577a80be1f330a4bec05e30367dbc3d
j_web <65983437+ancho4@users.noreply.github.com>
2021-02-12 13:43:01 -0500

    lab_3

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,1 +1,9 @@
 /* Put your javascript in here */
+let imgArray = ["onigiri_1.png", "onigiri_2.png", "onigiri_3.png", 
+"onigiri_4.png", "roll_1.png", "roll_2.png", "roll_3.png"]
+
+imgArray.forEach(element => { 
+    const img_item = document.createElement("img");
+    img_item.innerText = element; 
+})
+
