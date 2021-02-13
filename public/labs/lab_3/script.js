cd884ce618106a2ff4d7035631906650dbc26896
j_web <65983437+ancho4@users.noreply.github.com>
2021-02-12 22:34:38 -0500

    lab_3

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,9 +1,31 @@
-/* Put your javascript in here */
-let imgArray = ["onigiri_1.png", "onigiri_2.png", "onigiri_3.png", 
-"onigiri_4.png", "roll_1.png", "roll_2.png", "roll_3.png"]
+let i = 1;
+    for (let li of carousel.querySelectorAll('li')) {
+      li.style.position = 'relative';
+      li.insertAdjacentHTML('beforeend', `<span style="position:absolute;left:0;top:0">${i}</span>`);
+      i++;
+    }
 
-imgArray.forEach(element => { 
-    const img_item = document.createElement("img");
-    img_item.innerText = element; 
-})
+   
+    let width = 130; 
+    let count = 3; 
 
+    let list = carousel.querySelector('ul');
+    let listElems = carousel.querySelectorAll('li');
+
+    let position = 0; 
+
+    carousel.querySelector('.prev').onclick = function() {
+     
+      position += width * count;
+    
+      position = Math.min(position, 0)
+      list.style.marginLeft = position + 'px';
+    };
+
+    carousel.querySelector('.next').onclick = function() {
+     
+      position -= width * count;
+   
+      position = Math.max(position, -width * (listElems.length - count));
+      list.style.marginLeft = position + 'px';
+    };
